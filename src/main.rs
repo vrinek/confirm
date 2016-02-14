0e5ad2288f80d946701e9bc225ec742d3402677f
vrinek <vrinek@users.noreply.github.com>
2016-02-14 20:36:54 +0000

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,43 @@ fn main() {
     }
 }
 
+// Gets the answer from STDIN and trims it.
 fn get_answer() -> String {
     let mut answer = String::new();
 
     io::stdin().read_line(&mut answer)
         .expect("Failed to read line");
 
-    answer.trim().to_lowercase()
+    String::from(answer.trim())
 }
 
+// Returns true for a positive answer.
 fn is_positive(answer: String) -> bool {
+    let answer = answer.to_lowercase();
+
     answer == "yes" || answer == "y"
+}
+
+#[test]
+fn returns_true_for_positive() {
+    assert_eq!(is_positive(String::from("yes")), true);
+    assert_eq!(is_positive(String::from("y")), true);
+    assert_eq!(is_positive(String::from("YES")), true);
+    assert_eq!(is_positive(String::from("Y")), true);
+}
+
+#[test]
+fn returns_false_for_negative() {
+    assert_eq!(is_positive(String::from("no")), false);
+    assert_eq!(is_positive(String::from("n")), false);
+    assert_eq!(is_positive(String::from("NO")), false);
+    assert_eq!(is_positive(String::from("N")), false);
+}
+
+#[test]
+fn returns_false_by_default() {
+    assert_eq!(is_positive(String::from("")), false);
+    assert_eq!(is_positive(String::from("I'm not sure")), false);
+    assert_eq!(is_positive(String::from("maybe?")), false);
+    assert_eq!(is_positive(String::from("👍")), false);
 }
