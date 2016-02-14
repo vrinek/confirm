dd06360fd2172522dd49fe5c5f61f93928d4fe24
vrinek <vrinek@users.noreply.github.com>
2016-02-14 13:18:22 +0000

    Exit cleanly on "yes" and "y"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Are you sure? (yes/NO)");
+
+    let answer = get_answer();
+
+    if is_positive(answer) {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+fn get_answer() -> String {
+    let mut answer = String::new();
+
+    io::stdin().read_line(&mut answer)
+        .expect("Failed to read line");
+
+    answer.trim().to_lowercase()
+}
+
+fn is_positive(answer: String) -> bool {
+    answer == "yes" || answer == "y"
 }
